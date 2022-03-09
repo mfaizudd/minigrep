bdec8da2924b0bc530317cb4bd506825c3967c65
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2022-03-09 16:23:17 +0700

    refactor: extract run function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, fs, process, error::Error};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,10 +11,18 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong, when trying to read the file");
+    if let Err(e) = run(&config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.filename)?;
 
     println!("File contents:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
