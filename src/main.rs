24bb2d360355e2d8f59f0c8ea433b7f1d0a65ea3
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2022-03-09 15:10:16 +0700

    feat: reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +8,9 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong, when trying to read the file");
+
+    println!("File contents:\n{}", contents);
 }
