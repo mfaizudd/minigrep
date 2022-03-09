33c04688d16bb535fa539efbad453f05124bc99a
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2022-03-09 17:01:46 +0700

    feat: search fuction

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,13 @@ pub struct Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
 }
 
 impl Config {
