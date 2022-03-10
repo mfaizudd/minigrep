e85ec483503a84a6d62fe42f3aac2c84019dadaf
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2022-03-10 20:03:32 +0700

    feat: case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,47 @@
-use std::{error::Error, fs};
+use std::{error::Error, fs, env};
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duck";
         let contents = "\
 From troubles of the world
 I turn to ducks,
-Beautiful comical things";
+Beautiful comical things
+Duck supremacy";
 
         assert_eq!(vec!["I turn to ducks,"], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "DuCk";
+        let contents = "\
+From troubles of the world
+I turn to ducks,
+Beautiful comical things
+Duck supremacy";
+
+        assert_eq!(
+            vec!["I turn to ducks,", "Duck supremacy"],
+            search_case_insensitive(query, contents)
+        );
     }
 }
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let result = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in result {
         println!("{}", line);
     }
 
@@ -36,19 +58,33 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     result
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("Not enough arguments")
+            return Err("Not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config { query, filename, case_sensitive })
     }
 }
